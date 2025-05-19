427248075ab9d7bd7b91f4f76af9f9a1c14aecc0
KevinEraso <kerasom@unicauca.edu.co>
2025-05-19 12:09:39 -0500

    arreglo orders

--- src/controllers/orders.js
+++ src/controllers/orders.js
@@ -1,16 +1,69 @@
 const connection = require('../config/db');
-const { sucess, error } = require('../utils/requets');
 
-const getOrders = (req, res) => {
-    connection.query('SELECT * FROM pedidos', (err, results) => {
-        if (err) {
-            error(req, res, err);
-            return;
-        }
-        sucess(req, res, results);
+function getOrders(tabla) {
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT * FROM ${tabla}`, (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
+
+function getOrderById(id) {
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT * FROM pedidos WHERE id = ?`, [id], (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
+
+function createOrder(order) {
+    return new Promise((resolve, reject) => {
+        connection.query(`INSERT INTO pedidos SET ?`, order, (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
+
+function updateOrder(id, order) {
+    return new Promise((resolve, reject) => {
+        connection.query(`UPDATE pedidos SET ? WHERE id = ?`, [order, id], (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
+
+function deleteOrder(id) {
+    return new Promise((resolve, reject) => {
+        connection.query(`DELETE FROM pedidos WHERE id = ?`, [id], (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
     });
 }
 
 module.exports = {
-    getOrders
+    getOrders,
+    getOrderById,
+    createOrder,
+    updateOrder,
+    deleteOrder
 }
--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const connection = require('../config/db');
-const { getOrders} = require('../controllers/orders');
-const { sucess, error } = require('../utils/requets');
+//const connection = require('../config/db');
+const { getOrders } = require('../controllers/orders');
+//const { sucess, error } = require('../utils/requets');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    getOrders(req, res);
+    const all = getOrders('pedidos');
+    all.then((results) => {
+        res.status(200).json(results);
+    })
+    .catch((err) => {
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
